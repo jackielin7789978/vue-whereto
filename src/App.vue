8ac515a3a65e7db92b7949675f3df40494a5fb41
jackie <jackielin7789978@gmail.com>
2022-09-09 00:17:07 +0800

    project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="">今天，我想到臺東縣東河鄉都蘭去玩玩</div>
+</template>
+
+<style lang="scss">
+#app {
+	min-height: 100%;
+	background: #1d3557;
+	color: #fff;
+	font-family: 'Noto Sans TC', sans-serif;
+	font-weight: normal;
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
