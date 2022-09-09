598686377151a64f9ac8c3fd6b28c995e9f01bef
jackie <jackielin7789978@gmail.com>
2022-09-09 12:39:56 +0800

    縣市/鄉鎮區抽籤功能

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { TAIWAN_AREA } from './constants'
+import { getRandomArrayItem } from './utils'
+
+const targetCity = ref('')
+const targetDist = computed(() => {
+	return getRandomArrayItem(TAIWAN_AREA[targetCity.value])
+})
+
+const getRandomCity = () => {
+	const cities = Object.keys(TAIWAN_AREA)
+	targetCity.value = getRandomArrayItem(cities)
+}
+getRandomCity()
+</script>
 <template>
-	<div class="">今天，我想到臺東縣東河鄉都蘭去玩玩</div>
+	<div class="select-none flex items-center gap-x-8">
+		<div class="w-58">我想到 {{ targetCity }} {{ targetDist }} 去玩玩</div>
+		<button
+			@click="getRandomCity"
+			class="cursor-pointer px-4 py-1 rounded hover-transition bg-[#24436D] hover:(bg-[#192E4B] text-light-900) active:(bg-[#15263F])"
+		>
+			GO
+		</button>
+	</div>
 </template>
 
 <style lang="scss">
@@ -13,5 +37,9 @@
 	display: flex;
 	align-items: center;
 	justify-content: center;
+}
+
+.hover-transition {
+	@apply transition-all 0.5s ease;
 }
 </style>
